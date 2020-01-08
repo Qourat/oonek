35d3e0187ebabcef4369cb71448e2aafcbb72445
noman0121 <nomanshahid0121@gmail.com>
2020-01-08 12:39:59 -0500

    production config added

--- config/deploy.rb
+++ config/deploy.rb
@@ -20,6 +20,27 @@ set :deploy_to, "/home/deploy/#{fetch :application}"
 # Default value for :pty is false
 # set :pty, true
 
+
+namespace :deploy do
+  namespace :db do
+    desc "Load the database schema if needed"
+    task load: [:set_rails_env] do
+      on primary :db do
+        if not test(%Q[[ -e "#{shared_path.join(".schema_loaded")}" ]])
+          within release_path do
+            with rails_env: fetch(:rails_env) do
+              execute :rake, "db:schema:load"
+              execute :touch, shared_path.join(".schema_loaded")
+            end
+          end
+        end
+      end
+    end
+  end
+
+  before "deploy:migrate", "deploy:db:load"
+end
+
 # Default value for :linked_files is []
 # append :linked_files, 'config/database.yml', 'config/secrets.yml'
 
